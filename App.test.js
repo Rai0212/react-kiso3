c6e7a3918c2d25aed72dc0dba52a212f2d2e2e0f
TsukuiRai <rai0212lab@gmail.com>
2024-11-11 11:05:45 +0900

    1, 2 finished and reviced.

--- App.test.js
+++ App.test.js
@@ -5,7 +5,7 @@
 /* eslint-disable no-undef */
 
 // 参照：https://zenn.dev/crsc1206/articles/de79af226d0c69
-// 2時間くらいかかった．．．，だいぶ試行錯誤した．先人たちに感謝．
+// 2時間くらいかかった．．．，だいぶ試行錯誤した．
 
 import { render, screen } from '@testing-library/react';
 import App from './src/App';
@@ -13,7 +13,14 @@ import '@testing-library/jest-dom';
 
 test('renders email, password fields, and login button', () => {
   render(<App />);
-  expect(screen.getByPlaceholderText('Email')).toBeInTheDocument(); // メールの入力フォームがあること
-  expect(screen.getByPlaceholderText('Password')).toBeInTheDocument(); // パスワードの入力フォームがあること
+  const emailInput = screen.getByPlaceholderText('Email');
+  const passwordInput = screen.getByPlaceholderText('Password');
+
+  expect(emailInput).toBeInTheDocument(); // placeholderがEmailであるものが存在すること
+  expect(passwordInput).toBeInTheDocument(); // placeholderがPasswordであるものが存在すること
+
+  expect(emailInput).toHaveAttribute('type', 'text'); // emailInputのtypeがtextであること
+  expect(passwordInput).toHaveAttribute('type', 'password'); // passwordInputのtypeがpasswordであること
+
   expect(screen.getByRole('button', { name: "Login" })).toBeInTheDocument(); // "Login"と書いてあるボタンがあること
 });
--- tests/Login.test.js
+++ tests/Login.test.js
@@ -4,13 +4,13 @@ test('displays error message for invalid email input', async ({ page }) => {
   await page.goto('http://localhost:5173');
   
   // 不正なメールアドレスを入力
-  await page.fill('input[placeholder="Email"]', 'invalidemail');
-  await page.fill('input[placeholder="Password"]', 'password123');
+  await page.fill('input[placeholder="Email"]', 'AAA.gmail.com'); // @が含まれていないメールアドレスを入力．
+  await page.fill('input[placeholder="Password"]', '01234'); // 適当にパスワードを入力．
   
   // ログインボタンをクリック
   await page.click('button[type="submit"]');
   
   // エラーメッセージの表示を確認
   const errorMessage = await page.locator('text=Invalid email address').isVisible();
-  expect(errorMessage).toBe(true); // しっかりエラー文が出力されるかをここで確認
+  expect(errorMessage).toBe(true); // しっかりエラー文(Invalid email address)が出力されるかをここで確認
 });
